4a2a2f0759ed9ff2f0214af3389748ec12042c9e
masterkai <kai0919@hotmail.com>
2022-10-15 11:54:22 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       React app for deploy test
+       break change
       </header>
     </div>
   );
